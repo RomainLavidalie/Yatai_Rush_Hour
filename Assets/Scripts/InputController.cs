6e462bf08509b774145867edb539299de9678333
Romain Lavidalie <83449831+RomainLavidalie@users.noreply.github.com>
2021-12-17 11:22:23 +0100

    complete controller + shooting system

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public bool WantsToShoot = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WantsToShoot = Input.GetButton("Fire1");
+    }
+}
