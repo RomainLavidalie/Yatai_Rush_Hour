7f2019d614304a87605337c65f0988cb8909c2ff
JordanB <jordan.burnet@croozy.studio>
2022-06-12 16:13:22 +0200

    [ADD] Music to the menu and settings for the sound
    There is two mixer, one for the music in 2D and the other for the sound effects of the game.

---
+++ Assets/AudioSettings.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SetMusic()
+    {
+        mixer.SetFloat("musicVol", Mathf.Log(slider.value) * 20);
+    }
+    
+    public void SetSound()
+    {
+        mixer.SetFloat("soundVol", Mathf.Log(slider.value) * 20);
+    }
+}
+
