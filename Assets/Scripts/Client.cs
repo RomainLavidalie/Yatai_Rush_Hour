a9b42c1b55eb23cb99bec51b0224d511e8070d30
RoiMitha971 <emile.cometto@gmail.com>
2022-06-15 19:48:40 +0200

    AI can now check if it recieves the bowl it ordered
    functions to work on :
    Happy() : do everything you need when you hit with the correct bowl
    
    Angry(): // the wrong bowl, or something else

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -1,13 +1,48 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.XR;
+using Random = System.Random;
 
 public class Client : MonoBehaviour
 {
     public Recipe command;
+    private List<string> recievedBowl;
+    [SerializeField] IAStateMachine stateMachine;
+
+    private void Start()
+    {
+        Random rand = new Random();
+        command = RecipesManager.instance.recipesList[rand.Next(RecipesManager.instance.recipesList.Count)];
+    }
+
     public void OnTriggerEnter(Collider other)
     {
-        List<string> recieved = other.GetComponent<RamenBowl>().ingredientList;
+        try
+        {
+            recievedBowl = other.GetComponent<RamenBowl>().ingredientList;
+        }
+        catch
+        {
+            return;
+        }
+
+        if (Enumerable.SequenceEqual(recievedBowl.OrderBy(e => e), command.ingredients.OrderBy(e => e)))
+            Happy();
+        else
+            Angry();
+    }
+
+    private void Happy()
+    {
+        Debug.Log("bonne commande");
+        stateMachine._orderArrived = true;
+    }
+
+    private void Angry()
+    {
+        Debug.Log("mauvaise commande");
     }
 }
